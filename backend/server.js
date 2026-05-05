4fdf3c5ce3fc22c3ee5ea27a152834806a201fe4
Priya luhar <priyasaviluha@gmail.com>
2026-05-05 17:58:15 +0530

    Replace bcrypt with bcryptjs for Render compatibility

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
